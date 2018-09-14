9d101715c1c18dba86dee0040b83d00b21f240ca
mark-mansfield <markgmansfield@gmail.com>
2018-09-14 10:26:14 +1000

    added app-router

--- dorsal-angular-project/src/app/app.module.ts
+++ dorsal-angular-project/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CountriesComponent } from './countries/countries.component';
+
+const appRoutes: Routes = [
+  // route path   , comopnet to load
+  { path : '' , component: HomeComponent },
+  { path : 'countries' , component: CountriesComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
